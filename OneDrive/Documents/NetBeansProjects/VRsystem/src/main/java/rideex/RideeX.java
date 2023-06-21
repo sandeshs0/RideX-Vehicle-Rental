34a642ecff841d652f94988475dd63e27f5f1709
sandhyaNepal20 <117894362+sandhyaNepal20@users.noreply.github.com>
2023-06-21 12:24:55 +0545

    FRONT-END AND BACKEND COMPLETED

---
+++ OneDrive/Documents/NetBeansProjects/VRsystem/src/main/java/rideex/RideeX.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rideex;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class RideeX {
+    public static Connection dbconnect()
+    { 
+        try
+        {
+       Class.forName("com.mysql.cj.jdbc.Driver");
+       Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/ridex","root","@Sandhya20");
+            System.out.println("connected");
+            return conn;
+        }
+        catch(Exception e)
+        {
+            System.out.println("error");
+            return null;
+        }
+        
+    }
+    public static void main(String[] args) {
+        dbconnect();
+        System.out.println("run");
+                
+    }
+}
