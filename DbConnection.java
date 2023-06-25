e408cc699a9f448414e9b33d843839b709ffc62a
Yakuuuuuu <121224279+Yakuuuuuu@users.noreply.github.com>
2023-06-25 13:50:00 +0545

    Add files via upload

---
+++ DbConnection.java
@@ -0,0 +1,21 @@
+package billing;
+
+import java.sql.*;
+public class DbConnection {
+    public static Connection dbConnect()
+    {
+        try
+        {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/hyalo","root","Yaku12345");
+            System.out.println("connected to database");
+        return conn;
+        }
+        catch(Exception e)
+        {
+        }
+        return null;
+    }
+
+
+}
