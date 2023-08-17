a40a05d22c0d657f1a291843ee475e5845307e92
Sandesh Sapkota <86065388+sandeshs0@users.noreply.github.com>
2023-08-17 12:47:58 +0545

    Update BookingController.java

--- src/Controller/BookingController.java
+++ src/Controller/BookingController.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controller;
-
+import Model.BookingModel;
+import View.BookingsView;
+import DAO.BookingDAO;
+import javax.swing.JTable;
 /**
  *
  * @author hello
  */
 public class BookingController {
-    
+   public void setRecords(JTable tbl_vehicleData){
+       BookingDAO bdao=new BookingDAO();
+       bdao.setRecordsToTable(tbl_vehicleData);
+   }
+   public String[] checkCustomerRecord(String phoneNumber){
+      BookingDAO bdao=new BookingDAO();  
+      String[] cusDetails= bdao.checkCustomerRecord(phoneNumber);
+      return cusDetails;
+   }
 }
