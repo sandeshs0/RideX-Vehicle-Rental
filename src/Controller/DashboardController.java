5913b6291f2242f207b12f60670dccfd353eed0a
Sandesh Sapkota <86065388+sandeshs0@users.noreply.github.com>
2023-08-17 12:35:57 +0545

    Update DashboardController.java

--- src/Controller/DashboardController.java
+++ src/Controller/DashboardController.java
@@ -1,37 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
 
+import DAO.DashboardDAO;
 import DAO.dbConnection;
+import Model.DashboardModel;
+import View.DashboardView;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
-import javax.swing.table.TableRowSorter;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author hello
- */
 public class DashboardController {
-    private Connection conn;
-    private Statement stmt;
-    
-DefaultTableModel model;
- public DashboardController() {
-        conn = dbConnection.dbconnect();
+    DefaultTableModel model;
+    private DashboardView dashboardView;
+    private DashboardModel dashboardModel;
+    private DashboardDAO dashboardDAO;
+
+    public DashboardController(DashboardView dashboardView, DashboardModel dashboardModel, DashboardDAO dashboardDAO) {
+        this.dashboardView = dashboardView;
+        this.dashboardModel = dashboardModel;
+        this.dashboardDAO = dashboardDAO;
     }
 
-    public void setRecordsToTable(JTable tbl_vehicleData){
+    public void displayDashboard() {
+        dashboardView.setVisible(true);
+        updateDashboardData();
+    }
+
+    private void updateDashboardData() {
+        dashboardModel.setTotalVehicles(dashboardDAO.getTotalVehicles());
+        dashboardModel.setTotalBookings(dashboardDAO.getTotalBookings());
+        dashboardModel.setOnRentBookings(dashboardDAO.getOnRentBookings());
+        dashboardModel.setAvailableVehicles(dashboardDAO.getAvailableVehicles());
+        dashboardModel.setOverdueBookings(dashboardDAO.getOverdueBookings());
+
+        dashboardView.updateDashboard(dashboardModel);
+    }
+        public void setRecordsToTable(JTable tbl_vehicleData){
         try{
             Connection con = dbConnection.dbconnect();
-            PreparedStatement pst = con.prepareStatement("select * from vehicle");
+            java.sql.PreparedStatement pst = con.prepareStatement("select * from vehicle");
             ResultSet rs = pst.executeQuery();
             
             while(rs.next()){
@@ -40,20 +47,17 @@ DefaultTableModel model;
                 String vehiclemodel = rs.getString("model");
                 
                 String fuel = rs.getString("fuel");
-                String rate = rs.getString("rate");
-           
-
-                
-                
+                String rate = rs.getString("rate");   
                 Object[] obj = {vehiclebrand,vehiclemodel,fuel,rate};
                 model = (DefaultTableModel)tbl_vehicleData.getModel();
                 model.addRow(obj);
+                
             }
         
         
         }catch (Exception e) {
             e.printStackTrace();
+            
         }
     }
 }
-    
