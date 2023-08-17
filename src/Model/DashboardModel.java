50dd343553cbfb90248cb1e0813f55cc9f7eabec
Sandesh Sapkota <86065388+sandeshs0@users.noreply.github.com>
2023-08-17 13:05:41 +0545

    Update DashboardModel.java

--- src/Model/DashboardModel.java
+++ src/Model/DashboardModel.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Model;
 
-/**
- *
- * @author hello
- */
 public class DashboardModel {
-    
+    private int totalVehicles;
+    private int totalBookings;
+    private int onRentBookings;
+    private int availableVehicles;
+    private int overdueBookings;
+
+    // Getter and Setter methods for all fields
+
+    public int getTotalVehicles() {
+        return totalVehicles;
+    }
+
+    public void setTotalVehicles(int totalVehicles) {
+        this.totalVehicles = totalVehicles;
+    }
+
+    public int getTotalBookings() {
+        return totalBookings;
+    }
+
+    public void setTotalBookings(int totalBookings) {
+        this.totalBookings = totalBookings;
+    }
+
+    public int getOnRentBookings() {
+        return onRentBookings;
+    }
+
+    public void setOnRentBookings(int onRentBookings) {
+        this.onRentBookings = onRentBookings;
+    }
+
+    public int getAvailableVehicles() {
+        return availableVehicles;
+    }
+
+    public void setAvailableVehicles(int availableVehicles) {
+        this.availableVehicles = availableVehicles;
+    }
+
+    public int getOverdueBookings() {
+        return overdueBookings;
+    }
+
+    public void setOverdueBookings(int overdueBookings) {
+        this.overdueBookings = overdueBookings;
+    }
 }
