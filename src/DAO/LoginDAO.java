beddbc814521fe148f3f7c4477e7c0eaef95fdc3
Sandesh Sapkota <86065388+sandeshs0@users.noreply.github.com>
2023-08-17 12:53:23 +0545

    Update LoginDAO.java

--- src/DAO/LoginDAO.java
+++ src/DAO/LoginDAO.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
-/**
- *
- * @author hello
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class LoginDAO {
-    
+    private Connection conn;
+
+    public LoginDAO() {
+        conn = dbConnection.dbconnect();
+    }
+
+    public boolean validateLogin(String phone, String pass) {
+        try {
+            String query = "SELECT * FROM AdminDetails WHERE PhoneNumber=? AND pass=?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, phone);
+            stmt.setString(2, pass);
+            ResultSet resultSet = stmt.executeQuery();
+            return resultSet.next();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
