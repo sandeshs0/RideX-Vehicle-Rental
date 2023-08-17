69a4128602d4e2d939f59c38fcf63d1596affe0d
Sandesh Sapkota <86065388+sandeshs0@users.noreply.github.com>
2023-08-17 13:04:13 +0545

    Update BookingModel.java

--- src/Model/BookingModel.java
+++ src/Model/BookingModel.java
@@ -4,10 +4,121 @@
  */
 package Model;
 
-/**
- *
- * @author hello
- */
 public class BookingModel {
-    
+
+    public int getRate() {
+        return Rate;
+    }
+
+    public void setRate(int Rate) {
+        this.Rate = Rate;
+    }
+
+    public int getDays() {
+        return Days;
+    }
+
+    public void setDays(int Days) {
+        this.Days = Days;
+    }
+
+    public String getVechicleNumber() {
+        return VechicleNumber;
+    }
+
+    public void setVechicleNumber(String VechicleNumber) {
+        this.VechicleNumber = VechicleNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSpecial_request() {
+        return special_request;
+    }
+
+    public void setSpecial_request(String special_request) {
+        this.special_request = special_request;
+    }
+
+    public String getBorrow_date() {
+        return borrow_date;
+    }
+
+    public void setBorrow_date(String borrow_date) {
+        this.borrow_date = borrow_date;
+    }
+
+    public String getReturn_date() {
+        return return_date;
+    }
+
+    public void setReturn_date(String return_date) {
+        this.return_date = return_date;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_number() {
+        return customer_number;
+    }
+
+    public void setCustomer_number(String customer_number) {
+        this.customer_number = customer_number;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getCollateral() {
+        return collateral;
+    }
+
+    public void setCollateral(String collateral) {
+        this.collateral = collateral;
+    }
+
+    public String getAdvancePayment() {
+        return advancePayment;
+    }
+
+    public void setAdvancePayment(String advancePayment) {
+        this.advancePayment = advancePayment;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+private int Rate;
+    int Days;
+        private String VechicleNumber,brand,model,special_request,borrow_date,return_date,customer_name,customer_number,total,collateral,advancePayment;
+    private int bookingId;
 }
